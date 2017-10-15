6be75e80f3738cfb196d62505bbd0332be1d284b
nirmal <nirmaloasis@hotmail.com>
2017-10-16 01:08:26 +0530

    redux is awesome

--- src/components/courses/CoursePage.js
+++ src/components/courses/CoursePage.js
@@ -1,14 +1,55 @@
 import React from 'react'
-
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import * as courseActions from '../actions/courseAction'
 
 class CoursePage extends React.Component{
+    constructor(props,context){
+      super(props,context);
+      this.state = {
+          course:{title:""}
+      }
+      this.titleChange = this.titleChange.bind(this);
+      this.onClickSave = this.onClickSave.bind(this);
+    }
+    titleChange(event){
+        const {course} = this.state
+        course.title = event.target.value;
+        this.setState(course);
+    }
+    onClickSave(){
+      
+        this.props.actions.createCourse(this.state.course)
+    }
     render(){
         return(
             <div>
                 <h1>Course Page</h1>
-                
+                {this.props.courses.map((course,i) => <div key={i} >{course.title} </div>)}
+                <h2>Add Course</h2>
+                <input
+                  type="text"
+                  onChange = {this.titleChange}
+                  value={this.state.course.title} />
+                  <input
+                  type="submit"
+                  onClick = {this.onClickSave}
+                  value="save" />  
              </div>   
         )
     }
 }
-export default CoursePage
+
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        courses: state.courses
+    }
+}
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        actions: bindActionCreators(courseActions,dispatch)
+    };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CoursePage)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-//import 'bebel-polyfill'
+import 'babel-polyfill'
 import React from 'react'
 import {render} from 'react-dom'
 import {Router,browserHistory} from 'react-router'
-
+import {Provider} from 'react-redux'
+import configureStore from './components/store/configureStore'
 import routes from './routes'
 //import './styles/styles.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-render(<Router history={browserHistory} routes={routes}/>, document.getElementById('root'))
+const store = configureStore();
+
+render(   <Provider store={store}>
+           <Router history={browserHistory} routes={routes}/>
+           </Provider>, 
+document.getElementById('root'))
